1d8dc377f21778dadc63c856d93104ce3165600d
John Willis <jrwillis722@gmail.com>
2018-12-02 02:49:35 -0600

    Everything on frontend seems to work, but server side having difficulty fulfilling the tweetIt() func

--- resources/js/ajax.js
+++ resources/js/ajax.js
@@ -1,6 +1,22 @@
 'use strict';
 
-const submitPokemon = function(numTracks) {
+const tweetIt = function(url) {
+	const xhr = new XMLHttpRequest();
+	xhr.open('POST', '/tweet');
+	xhr.setRequestHeader('Content-Type', 'application/json');
+
+	xhr.addEventListener('load', function() {
+		console.log('tweeted it');
+	});
+
+	xhr.send(
+		JSON.stringify({
+			spotifyUrl: url
+		})
+	);
+};
+
+const getTracks = function(numTracks) {
 	const xhr = new XMLHttpRequest();
 	xhr.open('GET', '/get-music?numTracks=' + numTracks);
 
@@ -12,21 +28,23 @@ const submitPokemon = function(numTracks) {
 			let trackArtist = document.createTextNode(trackInfo.artists[0].name);
 			let trackUrl = trackInfo.external_urls.spotify;
 			//let trackPreview = trackInfo.preview_url;
-
 			let trackAnchor = document.createElement('a');
-
 			trackAnchor.setAttribute('href', trackUrl);
 			trackAnchor.innerText = trackName;
-
 			let tdArtist = document.createElement('td');
 			let tdName = document.createElement('td');
-
 			tdArtist.appendChild(trackArtist);
 			tdName.appendChild(trackAnchor);
+			let button = document.createElement('button');
+			button.appendChild(document.createTextNode('Tweet'));
+			button.setAttribute('onclick', `tweetIt('${trackUrl}')`);
+			let tdTwitterBtn = document.createElement('td');
+			tdTwitterBtn.appendChild(button);
 
 			let tr = document.createElement('tr');
 			tr.appendChild(tdArtist);
 			tr.appendChild(tdName);
+			tr.appendChild(tdTwitterBtn);
 
 			document.getElementById('dataTable').appendChild(tr);
 		});
@@ -51,6 +69,5 @@ document.querySelector('#musicInfo').addEventListener('submit', function(evt) {
 		numTracks = input.value;
 
 	input.value = '';
-	console.log(numTracks);
-	submitPokemon(numTracks);
+	getTracks(numTracks);
 });
