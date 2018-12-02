9b8d4a0c93011cb2bb95132a70a1eb3ac735fb8e
John Willis <jrwillis722@gmail.com>
2018-12-02 00:08:10 -0600

    Ajax call to get Spotify Recommendations and populate table w/ num of tracks requested by user

--- resources/js/ajax.js
+++ resources/js/ajax.js
@@ -1,11 +1,56 @@
 'use strict';
 
-const ajax = new XMLHttpRequest();
-ajax.open('GET', '/get-music?numberOfTracks');
+const submitPokemon = function(numTracks) {
+	const xhr = new XMLHttpRequest();
+	xhr.open('GET', '/get-music?numTracks=' + numTracks);
 
-ajax.addEventListener('load', function() {
-	console.log('Finished');
-	console.log(typeof ajax.response);
-	const track = JSON.parse(ajax.response);
+	xhr.addEventListener('load', function() {
+		const spotifyInfo = JSON.parse(xhr.response);
+		//loop over received data and dynamically add tracks to table
+		spotifyInfo.tracks.forEach(function(trackInfo) {
+			let trackName = trackInfo.name;
+			let trackArtist = document.createTextNode(trackInfo.artists[0].name);
+			let trackUrl = trackInfo.external_urls.spotify;
+			//let trackPreview = trackInfo.preview_url;
 
+			let trackAnchor = document.createElement('a');
+
+			trackAnchor.setAttribute('href', trackUrl);
+			trackAnchor.innerText = trackName;
+
+			let tdArtist = document.createElement('td');
+			let tdName = document.createElement('td');
+
+			tdArtist.appendChild(trackArtist);
+			tdName.appendChild(trackAnchor);
+
+			let tr = document.createElement('tr');
+			tr.appendChild(tdArtist);
+			tr.appendChild(tdName);
+
+			document.getElementById('dataTable').appendChild(tr);
+		});
+	});
+
+	xhr.addEventListener('error', function() {
+		console.error('Error occured :(');
+	});
+
+	xhr.timeout = 5 * 1000;
+	xhr.addEventListener('timeout', function() {
+		console.warn('Timeout');
+	});
+
+	xhr.send();
+};
+
+document.querySelector('#musicInfo').addEventListener('submit', function(evt) {
+	evt.preventDefault();
+
+	const input = document.querySelector('#input'),
+		numTracks = input.value;
+
+	input.value = '';
+	console.log(numTracks);
+	submitPokemon(numTracks);
 });
